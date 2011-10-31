c3eb1f5891fb68b5c51ecb2ca5b4fbc493a296f5
Shinya Amemiya <shinya@Ged.local>
2011-10-31 10:58:55 +0900

    Revert fb76a72059b964201f3bc14b3f3c86b146350ffe^..HEAD

--- main.m
+++ main.m
@@ -1,65 +1,39 @@
 
 #import <Foundation/Foundation.h>
 
-@class MyClass;
-@interface Composited : NSObject {
-	__weak MyClass *myClass_;
-}
-- (id)initWithMyClass:(MyClass *)myClass;
-- (void)func;
-@end
-
-@interface MyClass : NSObject {
-	Composited *composited_;
-}
-- (void)func;
+@interface Composited : NSObject
 @end
 
 @implementation Composited
-
-- (id)initWithMyClass:(MyClass *)myClass
-{
-	self = [super init];
-	if (self) {
-		myClass_ = myClass;
-	}
-	return self;
-}
-
 - (void)dealloc { NSLog(@"Composited's dealloc."); }
-
-- (void)func
-{
-	NSLog(@"Composited's func");
-	[myClass_ func];
-}
-
 @end
 
-@implementation MyClass
+@interface ObjC : NSObject {
+	Composited *composited_;
+}
+@end
+
+@implementation ObjC
 
 - (id)init
 {
     self = [super init];
     if (self) {
-        composited_ = [[Composited alloc] initWithMyClass:self];
+        composited_ = [[Composited alloc] init];
     }
     return self;
 }
 
 - (void)dealloc
 {
-	[composited_ func];
-	NSLog(@"MyClass's dealloc.");
+	NSLog(@"ObjC's dealloc.");
 }
-
-- (void)func { NSLog(@"MyClass's func"); }
 
 @end
 
 static void test()
 {
-	MyClass *mc = [[MyClass alloc] init];
+	ObjC *objc = [[ObjC alloc] init];
 }
 
 int main()
